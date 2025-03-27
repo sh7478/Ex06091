5dca9080f0d7769bba4d53a06e6bed52d0c3faa9
sh7478 <sh7478@bs.amalnet.k12.il>
2025-03-27 21:45:38 +0200

    End of coding

--- app/src/main/java/com/example/ex06091/MainActivity.java
+++ app/src/main/java/com/example/ex06091/MainActivity.java
@@ -1,18 +1,43 @@
 package com.example.ex06091;
 
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    WebView wV;
+    EditText etAdress;
+    String adress;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        wV = (WebView) findViewById(R.id.wV);
+        wV.getSettings().setJavaScriptEnabled(true);
+        etAdress = findViewById(R.id.etAddress);
+        /*adress = "";*/
     }
+
+    public void search(View view) {
+        wV.setWebViewClient(new MyWebViewClient());
+        adress = etAdress.getText().toString();
+        if (adress != "")
+        {
+            wV.loadUrl(adress);
+        }
+    }
+
+    private class MyWebViewClient extends WebViewClient {
+        public boolean shouldOverideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+    }
+
 }
